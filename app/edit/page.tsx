0818088f7576ac0d637a163f55b4cbe9bd1443a6
CodeTank <114135972+sanujasethi06@users.noreply.github.com>
2024-01-20 10:09:44 +0530

    Update page.tsx

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -87,7 +87,7 @@ export default function EditPage({
               src={publicId}
               width="1200"
               height="1400"
-              blur="800"
+{/*               blur="800" */}
               alt="some image"
             />
           )}
